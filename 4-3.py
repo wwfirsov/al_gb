a5c682e3e175ffeef2a1bea513ee3746c013956b
wwfirsov <65654471+wwfirsov@users.noreply.github.com>
2021-06-08 15:24:35 +0300

    dz4
    dz4

---
+++ 4-3.py
@@ -0,0 +1,67 @@
+"""
+Задание 3.
+Приведен код, формирующий из введенного числа
+обратное по порядку входящих в него
+цифр и вывести на экран.
+Сделайте профилировку каждого алгоритма через cProfile и через timeit
+Обязательно предложите еще свой вариант решения и также запрофилируйте!
+Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
+Без аналитики задание считается не принятым
+"""
+import cProfile
+from timeit import timeit
+
+# Рекурсия
+def revers_1(enter_num, revers_num=0):
+    if enter_num == 0:
+        return revers_num
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        return revers_1(enter_num, revers_num)
+
+# Цикл
+def revers_2(enter_num, revers_num=0):
+    while enter_num != 0:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+# Срез
+def revers_3(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    return revers_num
+
+enter_num = int(input('Введите целое число: '))
+
+revers_1(enter_num)
+revers_2(enter_num)
+revers_3(enter_num)
+
+print(
+    'Число наоборот на рекурсиях: ',
+    timeit(
+        f'revers_1({enter_num})',
+        globals=globals(),
+        number=10000))
+
+print(
+    'Число наоборот на циклах: ',
+    timeit(
+        f'revers_2({enter_num})',
+        globals=globals(),
+        number=10000))
+
+print(
+    'Число наоборот на срезах: ',
+    timeit(
+        f'revers_3({enter_num})',
+        globals=globals(),
+        number=10000))
+
+cProfile.run('revers_1(10000000000)')
+cProfile.run('revers_2(10000000000)')
+cProfile.run('revers_3(10000000000)')
